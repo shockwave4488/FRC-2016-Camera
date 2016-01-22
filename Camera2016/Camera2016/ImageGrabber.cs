0b30461ce316b417b6b61dbd7c6836d6b50de74b
lessthantrue <nickm@outofthisworld.net>
2016-01-22 11:49:01 -0800

    Initial Camera commit

---
+++ Camera2016/Camera2016/ImageGrabber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Emgu.CV;
+using Emgu.CV.UI;
+
+namespace CameraThing
+{
+    class ImageGrabber
+    {
+        private Capture m_grabber;
+        private Mat m_buf1, m_buf2;
+        private bool m_switch;
+        private Thread m_captureThread;
+
+        public ImageGrabber()
+        {
+            m_grabber = new Capture("http://10.44.88.11/axis-cgi/mjpg/video.cgi?resolution=320x240&.mjpg");
+            m_switch = false;
+            m_captureThread = new Thread(run);
+            m_captureThread.Start();
+        }
+
+        private void run()
+        {
+            while (true)
+            {
+                Mat image = m_grabber.QueryFrame();
+                if (m_switch)
+                    m_buf1 = image;
+                else
+                    m_buf2 = image;
+
+                m_switch = !m_switch;
+            }
+        }
+
+        public Mat Image()
+        {
+            return !m_switch ? m_buf1 : m_buf2;
+        }
+    }
+}
---
+++ Camera2016/Camera2016/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using System.Windows.Forms;
+
+namespace CameraThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImageViewer viewer = new ImageViewer();
+
+            ImageGrabber i = new ImageGrabber();
+
+            Application.Idle += (o, s) =>
+            {
+                Mat image = i.Image();
+                Mat output = new Mat();
+                Mat hsv = new Mat();
+
+                if (null == image)
+                    return;
+
+                CvInvoke.CvtColor(image, hsv, Emgu.CV.CvEnum.ColorConversion.Bgr2Hsv);
+                MCvScalar low = new MCvScalar(81, 103, 186);
+                MCvScalar high = new MCvScalar(96, 255, 255);
+                CvInvoke.InRange(hsv, new ScalarArray(low), new ScalarArray(high), output);
+                //cvtColor(input, hsv, COLOR_BGR2HSV);
+                //inRange(hsv, low, high, output);
+                //outputSocket.setValue(output);
+
+                viewer.Image = output;
+
+                //Thread.Sleep(100);
+            };
+
+            viewer.ShowDialog();
+        }
+    }
+}
