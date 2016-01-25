f543591c65c649c8714b0f029c58b34e70437ca7
lessthantrue <nickm@outofthisworld.net>
2016-01-24 17:29:40 -0800

    Contours
    Forgot to save

--- Camera2016/Camera2016/Program.cs
+++ Camera2016/Camera2016/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
@@ -8,6 +9,10 @@ using Emgu.CV;
 using Emgu.CV.UI;
 using Emgu.CV.Structure;
 using System.Windows.Forms;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using System.Drawing;
+
 
 namespace CameraThing
 {
@@ -23,22 +28,24 @@ namespace CameraThing
             {
                 //Switch between buf1 and buf2 when passing by reference
                 //use output on the final operation
-                Mat buf1 = i.Image();
-                Mat buf2 = new Mat();
+                Mat image = i.Image();
+                Mat HsvIn = new Mat();
+                Mat HsvOut = new Mat();
                 Mat output = new Mat();
 
-                if (null == buf1)
+                if (null == image)
                     return;
 
                 //HSV Filter
-                CvInvoke.CvtColor(buf1, buf2, Emgu.CV.CvEnum.ColorConversion.Bgr2Hsv);
+                CvInvoke.CvtColor(image, HsvIn, Emgu.CV.CvEnum.ColorConversion.Bgr2Hsv);
                 MCvScalar low = new MCvScalar(63, 44, 193);
                 MCvScalar high = new MCvScalar(97, 255, 255);
-                CvInvoke.InRange(buf2, new ScalarArray(low), new ScalarArray(high), output);
+                CvInvoke.InRange(HsvIn, new ScalarArray(low), new ScalarArray(high), HsvOut);
 
                 //Contours
-
-
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                CvInvoke.FindContours(HsvOut, contours, null, RetrType.List, ChainApproxMethod.ChainApproxTc89Kcos);
+                
                 //Filter contours
 
 
