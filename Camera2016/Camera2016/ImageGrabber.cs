be5a57dca591089dfbc1e3c21fc88dd025906bcd
lessthantrue <nickm@outofthisworld.net>
2016-01-27 21:14:49 -0800

    Filtering and memory

--- Camera2016/Camera2016/ImageGrabber.cs
+++ Camera2016/Camera2016/ImageGrabber.cs
@@ -13,7 +13,8 @@ namespace Camera2016
         /// </summary>
         private Capture m_grabber;
 
-        private Mat m_buf1, m_buf2;
+        private readonly object m_mutex = new object();
+        private Mat m_buf1 = new Mat(), m_buf2 = new Mat();
         private bool m_switch;
         private Thread m_captureThread;
 
@@ -22,8 +23,8 @@ namespace Camera2016
         /// </summary>
         public ImageGrabber()
         {
-            //m_grabber = new Capture("http://10.44.88.11/axis-cgi/mjpg/video.cgi?resolution=320x240&.mjpg");
-            m_grabber = new Capture();
+            m_grabber = new Capture("http://10.44.88.11/axis-cgi/mjpg/video.cgi?resolution=320x240&.mjpg");
+            //m_grabber = new Capture();
             m_switch = false;
             m_captureThread = new Thread(run);
             m_captureThread.Start();
@@ -33,13 +34,18 @@ namespace Camera2016
         {
             while (true)
             {
-                Mat image = m_grabber.QueryFrame();
-                if (m_switch)
-                    m_buf1 = image;
-                else
-                    m_buf2 = image;
+                m_grabber.Grab();
+                Mat image;
+                lock (m_mutex)
+                {
+                    if (m_switch)
+                        image = m_buf1;
+                    else
+                        image = m_buf2;
 
-                m_switch = !m_switch;
+                    m_switch = !m_switch;
+                }
+                m_grabber.Retrieve(image);
             }
         }
 
@@ -49,7 +55,10 @@ namespace Camera2016
         /// <returns></returns>
         public Mat Image()
         {
-            return !m_switch ? m_buf1 : m_buf2;
+            lock (m_mutex)
+            {
+                return m_switch ? m_buf1.Clone() : m_buf2.Clone();
+            }
         }
     }
 }
--- Camera2016/Camera2016/Program.cs
+++ Camera2016/Camera2016/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
@@ -24,7 +25,7 @@ namespace Camera2016
                 Mat output = new Mat();
                 //output.Create(image.Height, image.Width, DepthType.Cv8U, 1); This is giving me problems for some reason
 
-                if (null == image)
+                if (image.IsEmpty)
                     return;
 
                 //HSV Filter
@@ -33,30 +34,42 @@ namespace Camera2016
                 MCvScalar high = new MCvScalar(97, 255, 255);
                 CvInvoke.InRange(HsvIn, new ScalarArray(low), new ScalarArray(high), HsvOut);
 
+                Mat Temp = new Mat();
+                HsvOut.ConvertTo(Temp, DepthType.Cv8U);
                 //Contours
                 VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
-                CvInvoke.FindContours(HsvOut, contours, null, RetrType.List, ChainApproxMethod.ChainApproxTc89Kcos);
-                CvInvoke.DrawContours(output, contours, -1, new MCvScalar(0, 0, 0));
-                
-                //Filter contours
-                VectorOfVectorOfPoint filteredContours = new VectorOfVectorOfPoint();
+                CvInvoke.FindContours(Temp, contours, null, RetrType.List, ChainApproxMethod.ChainApproxTc89Kcos);
+                //CvInvoke.DrawContours(output, contours, -1, new MCvScalar(0, 0, 0));
+
+                VectorOfVectorOfPoint convexHulls = new VectorOfVectorOfPoint(filteredContours.Size);
                 for (int i = 0; i < contours.Size; i++)
                 {
-                    VectorOfPoint contour = contours[i];
+                    CvInvoke.ConvexHull(contours[i], convexHulls[i]);
+                }
+                //Filter contours
+                VectorOfVectorOfPoint filteredContours = new VectorOfVectorOfPoint();
+                for (int i = 0; i < convexHulls.Size; i++)
+                {
+                    VectorOfPoint contour = convexHulls[i];
+                    VectorOfPoint polygon = new VectorOfPoint(convexHulls.Size);
+                    CvInvoke.ApproxPolyDP(contour, polygon, 10, true);
+                    if (polygon.Size != 4) continue;
+                    if (!CvInvoke.IsContourConvex(polygon)) continue;
+                    for (int j = 0; j < 4; j++)
+                    {
+                        double dx = polygon[j].X - polygon[(j - 1)%4].X;
+                        double dy = polygon[j].Y - polygon[(j - 1) % 4].Y;
+                        double slope = double.MaxValue;
+                        if (dx != 0) Math.Abs(dy / dx);
+                        double nearlyHorizontalSlope = Math.Tan(ToRadians(20));
+                        double nearlyVerticalSlope = Math.Tan(ToRadians(70));
+                    }  
                     //Rectangle r = CvInvoke.BoundingRectangle(contour); <- use if we need min/max width/height
 
                     if (CvInvoke.ContourArea(contour) < 100) continue;
 
                     filteredContours.Push(contour);
                 }
-
-                //Convex hull
-                VectorOfVectorOfPoint convexHulls = new VectorOfVectorOfPoint(filteredContours.Size);
-                for (int i = 0; i < filteredContours.Size; i++)
-                {
-                    CvInvoke.ConvexHull(filteredContours[i], convexHulls[i]);
-                }
-                
                 //report to NetworkTables
 
 
@@ -65,6 +78,11 @@ namespace Camera2016
             };
 
             viewer.ShowDialog();
+        }
+
+        private static double ToRadians(double degrees)
+        {
+            return degrees*Math.PI/180.0;
         }
     }
 }
